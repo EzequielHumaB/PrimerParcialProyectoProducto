eb8b29e7de84c5429a4c5ebeb8e09b721c820f1f
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-02 18:01:37 -0400

    Agregando la funcion Guardar en la funcion TotalInventarioClase en la carpeta BLL

--- ProyectoParcialProductos/BLL/TotalInventarioClase.cs
+++ ProyectoParcialProductos/BLL/TotalInventarioClase.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ProyectoParcialProductos.DAL;
+using ProyectoParcialProductos.Entidades;
+using System.Data.Entity;
 
 namespace ProyectoParcialProductos.BLL
 {
    public class TotalInventarioClase
     {
-
+        public static bool Guardar(TotalInventario totalInventario)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.totalInventarios.Add(totalInventario)!=null)
+                {
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }catch(Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
     }
 }
