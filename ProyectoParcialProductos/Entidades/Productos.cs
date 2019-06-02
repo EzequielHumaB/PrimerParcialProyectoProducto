45e7e429fc4af93a8eefa38309893ad8a246346e
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-02 12:44:56 -0400

    Creacion de la clase Productos, en la carpeta de Entidades, que va a representar la tabla en la base de datos

--- ProyectoParcialProductos/Entidades/Productos.cs
+++ ProyectoParcialProductos/Entidades/Productos.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
 
-namespace ProyectoParcialProductos.DAL
+namespace ProyectoParcialProductos.Entidades
 {
-   public class Contexto
+    public class Productos
     {
     }
 }
