12f3aff90e16d30cd609139d5f5c343114686ca9
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-02 17:43:06 -0400

    Agregando las propiedades de TotalInventario y su respectivo constructor

--- ProyectoParcialProductos/Entidades/TotalInventario.cs
+++ ProyectoParcialProductos/Entidades/TotalInventario.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoParcialProductos.Entidades
 {
    public class TotalInventario
     {
+        [Key]
+        public int ProductoID { get; set; }
+
+        public decimal InventarioTotal { get; set; }
+     
+        public TotalInventario()
+        {
+            InventarioTotal = 0;
+        }
     }
 }
