6802db8ec9fde9ea66770f2096cf99a144844ce5
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-02 12:48:14 -0400

    Agregando las propiedades de la clase Productos, con su respectivo constructor

--- ProyectoParcialProductos/Entidades/Productos.cs
+++ ProyectoParcialProductos/Entidades/Productos.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoParcialProductos.Entidades
 {
     public class Productos
     {
+        [Key]
+        public int ProductoID { get; set; }
+        public string Descripcion { get; set; }
+        public int existencia { get; set; }
+        public double costo { get; set; }
+        public double ValorInventario { get; set; }
+
+        public Productos()
+        {
+            ProductoID = 0;
+            Descripcion = string.Empty;
+            existencia = 0;
+            costo = 0.0d;
+            ValorInventario = 0.0d;
+        }
+
     }
 }
