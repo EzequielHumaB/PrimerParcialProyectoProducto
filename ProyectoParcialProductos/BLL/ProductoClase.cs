bde0b58413281486a1dff9780a5bfcbf81a010b2
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-02 12:57:09 -0400

    Creando el metodo Guardar en la clase ProductoClase de la carpeta BLL

--- ProyectoParcialProductos/BLL/ProductoClase.cs
+++ ProyectoParcialProductos/BLL/ProductoClase.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProyectoParcialProductos.DAL;
+using ProyectoParcialProductos.Entidades;
 
 namespace ProyectoParcialProductos.BLL
 {
-    class ProductoClase
+   public class ProductoClase
     {
+        public static bool Guardar(Productos productos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.productos.Add(productos)!=null)
+                {
+                    paso = contexto.SaveChanges() > 0;
+                }
+            } catch(Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
     }
 }
